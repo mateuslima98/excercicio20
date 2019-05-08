0afa2e61772bb7f5a8e827c56569719a2b9e7c86
Mateus de Lima <mateus_lima98@hotmail.com>
2019-05-08 19:50:03 -0300

    Ex 19 - Revisão de JPA

---
+++ src/main/java/br/cometechoracio/excercicio20/Excercicio20Application.java
@@ -0,0 +1,13 @@
+package br.cometechoracio.excercicio20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Excercicio20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Excercicio20Application.class, args);
+	}
+
+}
